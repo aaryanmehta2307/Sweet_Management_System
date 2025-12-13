c2fc21fe9af82459d649b49453a9b5d52273e578
aaryanmehta2307 <118744251+aaryanmehta2307@users.noreply.github.com>
2025-12-13 16:46:51 +0530

    failed-test-case-added-and-tested-for-admin-role

--- Backend/src/tests/sweet.test.js
+++ Backend/src/tests/sweet.test.js
@@ -33,6 +33,44 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
+let adminToken;
+
+beforeAll(async () => {
+  await mongoose.connect(process.env.MONGO_URI);
+  await User.deleteMany({});
+  await Sweet.deleteMany({});
+
+  // normal user
+  await request(app).post("/api/auth/register").send({
+    name: "User",
+    email: "user@test.com",
+    password: "password123"
+  });
+
+  const userLogin = await request(app).post("/api/auth/login").send({
+    email: "user@test.com",
+    password: "password123"
+  });
+
+  token = userLogin.body.token;
+
+  // admin user
+  await User.create({
+    name: "Admin",
+    email: "admin@test.com",
+    password: await require("bcryptjs").hash("admin123", 10),
+    role: "admin"
+  });
+
+  const adminLogin = await request(app).post("/api/auth/login").send({
+    email: "admin@test.com",
+    password: "admin123"
+  });
+
+  adminToken = adminLogin.body.token;
+});
+
+
 describe("Sweet API", () => {
   it("should add a new sweet", async () => {
     const res = await request(app)
@@ -68,6 +106,37 @@ describe("Sweet API", () => {
   expect(res.body.length).toBeGreaterThan(0);
   expect(res.body[0]).toHaveProperty("category", "Indian");
 });
+
+it("should NOT allow normal user to delete a sweet", async () => {
+  const sweets = await request(app)
+    .get("/api/sweets")
+    .set("Authorization", `Bearer ${token}`);
+
+  const sweetId = sweets.body[0]._id;
+
+  const res = await request(app)
+    .delete(`/api/sweets/${sweetId}`)
+    .set("Authorization", `Bearer ${token}`);
+
+  expect(res.statusCode).toBe(403);
+});
+
+it("should allow admin to delete a sweet", async () => {
+  const sweet = await Sweet.create({
+    name: "Barfi",
+    category: "Indian",
+    price: 30,
+    quantity: 10
+  });
+
+  const res = await request(app)
+    .delete(`/api/sweets/${sweet._id}`)
+    .set("Authorization", `Bearer ${adminToken}`);
+
+  expect(res.statusCode).toBe(200);
+});
+
+
 
 
 });
