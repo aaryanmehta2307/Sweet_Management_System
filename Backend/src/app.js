aa4d4bd4f941602e250dbb4fb3b14b518dd68881
aaryanmehta2307 <118744251+aaryanmehta2307@users.noreply.github.com>
2025-12-13 14:54:05 +0530

    Backend-setup

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Sweet Shop API is running 🚀");
+});
+
+module.exports = app;
